68d205a7a8c4f3d66d05b286503d72e12c93f5b4
valeri832 <wpgnik@gmail.com>
2024-12-13 09:52:07 +0300

    Add files via upload

---
+++ module_8_2.py
@@ -0,0 +1,29 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+
+    for item in numbers:
+        try:
+            result += item
+        except TypeError:
+            print(f'Некорректный тип данных для подсчёта суммы - {item}')
+            incorrect_data += 1
+
+    return result, incorrect_data
+
+def calculate_average(numbers):
+    try:
+        result, incorrect_data = personal_sum(numbers)
+        count = len(numbers) - incorrect_data
+        return result / count if count > 0 else 0
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+        return None
+
+# Примеры вызова функций
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
